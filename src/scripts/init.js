323ac6c3d37d783d559ae90f8ee682289b10876e
Claudio Campos <claudio@claudiocampos.com>
2015-03-28 18:26:10 -0400

    create LineGraph instance and add sample data

---
+++ src/scripts/init.js
@@ -0,0 +1,22 @@
+(function () {
+    configuration = {
+        height: 175,
+        width: 575,
+        firstPropertyName: 'age',
+        secondPropertyName: 'nextBirthday',
+        data: [
+            {
+                age: 22,
+                nextBirthday: '2015-12-01',
+                marketCap: 124000
+            },
+            {
+                age: 26,
+                nextBirthday: '2015-06-14',
+                marketCap: 'na'
+            }
+        ]
+    }
+
+    var lineGraph = new LineGraph(configuration);
+})();
